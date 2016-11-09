3a76e18ceb54546ae306b95ac9f2e5637750a428
Jaclyn Rudolf <Jaclyn@Jaclyns-MacBook-Air.local>
2016-11-09 12:42:57 -0500

    first commit with parts of homework done

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -10,3 +10,28 @@
 
 # Deliverables
 # Make sure the new page is uploaded to your GitHub account.
+
+import requests 
+from bs4 import BeautifulSoup 
+import re
+import urllib.request, urllib.parse, urllib.error
+base_url = 'https://www.si.umich.edu/programs/bachelor-science-information/bsi-admissions'
+html = urllib.request.urlopen(base_url).read() 
+soup = BeautifulSoup(html, 'html.parser') 
+
+#need to convert the object to string
+txt = soup.prettify() 
+
+for word in txt:
+	find_string = soup.body.findAll(text=re.compile('student')
+print str.replace('student', 'AMAZING student')
+
+#for student in soup.find_all(class_='body-inside2'): 
+
+
+#swap w pic after   .html is the webpage (chapter 7)
+webpage = open('file.html', 'w')
+webpage.writetxt 
+
+
+#theres a lot of ways to replace findall or regex
--- HW3-StudentCopy/twitterhw3b.py
+++ HW3-StudentCopy/twitterhw3b.py
@@ -8,5 +8,57 @@
 # Be prepared to change the search term during demo.
 
 
+# Write a Python file that uploads an image to your 
+# Twitter account.  Make sure to use the 
+# hashtags #UMSI-206 #Proj3 in the tweet.
+
+# You will demo this live for grading.
+
+print("""No output necessary although you 
+	can print out a success/failure message if you want to.""")
+
+
+
+#why not these
+
+import requests
+import json
+
+#google 
+api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+file = open('Your_image.png', 'rb')
+data = file.read()
+r = api.request('statuses/update_with_media', {'status':'Your tweet'}, {'media[]':data})
+print(r.status_code)
+shareimprove this answer
+
+#real 
+
+import tweepy import 0AuthHandler
+from tweepy import API
+from tweepy import Cursor
+from textblob import textblob
+
+consumer_key = "I1NE9EwNVekvFYkeiiwaoT3iZ"
+consumer_secret = "injoKb5LWmAekKH9ALdscuG9B00Gs456kKi3Ykt2uwD3OD5QVM"
+access_token = "250423934-mYtCr5i8RHFAN6bXl3sM9QxzgvNeqyazq19BlTbq"
+access_secret = "J7rhELXeHkPQ6xDfMtqtxqrctiCmzCO8ZimcZrHnQ7ZDU"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+tweet_lst = []
+search_term = input('Input a term: ')
+for tweets in tweepy.Cursor(api.search, q=search_term, result_type='recent', include_entities=True, lang='en', count=100).items(100): 
+	print (tweets.text)
+
+analysis = TextBlob(tweets.text)
+print (analysis.sentiment)
+
+
+
+
 print("Average subjectivity is")
 print("Average polarity is")
